58166cf6f26a809358b24a0692cf41adec906769
fallenhh <fallen2hh@gmail.com>
2019-04-25 21:28:47 +0800

    Init the repo

--- src/index.js
+++ src/index.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
-import styles from './styles.css'
+import cplayer from 'cplayer'
 
 export default class ExampleComponent extends Component {
   static propTypes = {
     text: PropTypes.string
   }
 
-  render() {
-    const {
-      text
-    } = this.props
+  static defaultProps = {
+    onInit() { },
+    playlist: [
+      {
+        src: 'https://music.163.com/song/media/outer/url?id=500684228.mp3',
+        poster: 'http://p2.music.126.net/noJPpaixwhM7vS7Mfhs9sg==/109951163008620133.jpg',
+        name: 'Discovery',
+        artist: 'Electro-Light',
+        lyric: '',
+        sublyric: '',
+        album: 'Discovery'
+      },
+    ],
+    dark: false,
+    big: false,
+  }
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+  componentDidMount() {
+    const { onInit, ...restProps } = this.props;
+    const control = new cplayer({
+      ...restProps,
+      element: this.container
+    });
+    this.control = control;
+    onInit(control);
+  }
+
+  render() {
+    return <div ref={el => (this.container = el)} />;
   }
 }
